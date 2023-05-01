5e2f99305d4b6655577b747cd413e945aba8db59
Moises Morais <mika.devs100@gmail.com>
2023-05-01 20:30:13 -0300

    added contact section

--- src/js/main.js
+++ src/js/main.js
@@ -40,7 +40,33 @@ const blurHeader = () =>{
 window.addEventListener('scroll', blurHeader)
 
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+        contactMessage = document.getElementById('contact-message')
 
+const sendEmail = (e) => {
+    e.preventDefault()
+
+    //serviceID - TemplateID - #form - publicKey
+    emailjs.sendForm('serviceID', 'TemplateID', '#contact-form', 'key')
+    .then(() => {
+        //Show sent message
+        contactMessage.textContent = 'Message sent successfully ✅'
+
+        //Remove message after five seconds
+        setTimeout(() => {
+            contactMessage.textContent = ''
+        }, 5000)
+
+        //Clear fields
+        contactForm.reset()
+
+    }, () => {
+        //Show error message
+        contactMessage.textContent = 'Message not sent (service unvailable) ❌'
+    })
+}
+
+contactForm.addEventListener('submit', sendEmail)
 
 /*=============== SHOW SCROLL UP ===============*/ 
 
