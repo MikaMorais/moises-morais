da26ae442a59c458c919a1d405a9a194735a173e
Moises Morais <mika.devs100@gmail.com>
2023-05-13 17:31:57 -0300

    add: seção de serviços

--- src/js/main.js
+++ src/js/main.js
@@ -9,7 +9,27 @@ window.addEventListener('scroll', scrollHeader)
 
 
 /*=============== SERVICES MODAL ===============*/
+const modalViews = document.querySelectorAll('.services__modal')
+    modalButtons = document.querySelectorAll('.services__button'),
+    modalClose = document.querySelectorAll('.services__modal-close')
+    
+let modal = function(modalClick) {
+    modalViews[modalClick].classList.add('active-modal')
+}
 
+modalButtons.forEach((mbutton, i)  => {
+    mbutton.addEventListener('click', () => {
+        modal(i)
+    })
+});
+
+modalClose.forEach((mclose)  => {
+    mclose.addEventListener('click', () => {
+        modalViews.forEach((mview) => {
+            mview.classList.remove('active-modal')
+        })
+    })
+});
 
 /*=============== MIXITUP FILTER PORTFOLIO ===============*/
 
